78accef58d33497c0b7d426f0e061b62fea0e6c0
PennTao <whu.eis.lt@gmail.com>
2014-03-15 19:45:08 -0400

    CircusTower with DP

--- CircusTower/CircusTower.py
+++ CircusTower/CircusTower.py
@@ -2,9 +2,38 @@
 #For practical and aesthetic reasons, each person must be both shorter and lighter than the person below him or her.
 #Given the heights and weights of each person in the circus, write a method to compute
 #the largest possible number of people in such a tower
+import copy
 
-class CircusTower(self):
-    def BuildLargestTower(self, people):
-        if (len(people) == 0 or people == None):
-            return []
+class CircusTower:
+    def BuildLargestTowerHelper(self, people,person, mRec):
+        if (len(people) == 0 or people == None or person == None):
+            return
+        if(person in mRec):
+            return mRec[person]
+        maxCount = 0
+        maxTower = []                
+        for curP in people:
+            if curP[0] < person[0] and curP[1] < person[1]:
+                curTower = self.BuildLargestTowerHelper(people,curP,mRec)
+                if curTower != None:
+                    if len(curTower) > maxCount:
+                        #maxTower = copy.deepcopy(curTower)
+                        #maxTower = copy.copy(curTower)
+                        maxTower = curTower
+                        maxCount = len(curTower)
+        res = [person]
+        mRec[person] = maxTower + res
+        return mRec[person]
 
+    def BuildLargestTower(self,people):
+        maxCount = 0
+        maxTower = []
+        mRec = dict()
+        for person in people:
+            curTower = self.BuildLargestTowerHelper(people,person , mRec)
+            if curTower != None:
+                if len(curTower) > maxCount:
+                    maxCount = len(curTower)
+                    maxTower = curTower
+        
+        return maxTower
--- CircusTower/TestCircusTower.py
+++ CircusTower/TestCircusTower.py
@@ -1,17 +1,19 @@
-import uniitest
+import unittest
 from CircusTower import *
 
 class TestCircusTower(unittest.TestCase):
     def test_BuildLargestTower(self):
         people1 =[(65,170)]
         people2 = []
-        people3 = [(70,180),(55,160),(50,159),(65,170),(60,169)]
-        people4 = [(66,171),(66,171),(66,171)]
-        blt = BuildLargestTower()
-        self.assertEqual([(65,170)], blt.BuildLargestTower(people1))
-        self.assertEqual([], blt.BuildLargestTower(people2))
-        self.assertEqual([(50,159),(55,160),(60,169),(65,170),(70,180)], blt.BuildLargestTower(people3))
-        self.assertEqual([(66,171)], blt.BuildLargestTower(people4))
+        people3 = [(65,170),(66,171)]
+        people4 = [(70,180),(55,160),(50,159),(65,170),(60,169)]
+        people5 = [(66,171),(66,171),(66,171)]
+        ct = CircusTower()
+        self.assertEqual([(65,170)], ct.BuildLargestTower(people1))
+        self.assertEqual([], ct.BuildLargestTower(people2))
+        self.assertEqual([(65,170),(66,171)], ct.BuildLargestTower(people3))
+        self.assertEqual([(50,159),(55,160),(60,169),(65,170),(70,180)], ct.BuildLargestTower(people4))
+        self.assertEqual([(66,171)], ct.BuildLargestTower(people5))
 if __name__ == '__main__':
     unittest.main()
 
